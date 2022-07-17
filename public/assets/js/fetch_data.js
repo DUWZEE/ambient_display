af35813e43fce8649e492dd58b6ef5553d07526b
duwzee <duwzee@gmail.com>
2022-07-17 14:58:41 +0200

    design

--- public/assets/js/fetch_data.js
+++ public/assets/js/fetch_data.js
@@ -11,16 +11,35 @@ const w_place = document.getElementById('w_place')
 const w_img = document.getElementById('w_img')
 const w_urlbase = "../../assets/img/weather/"
 
+const s_image = document.getElementById('thumbnail_img')
+const s_artist = document.getElementById('artistname')
+const s_track = document.getElementById('track_name')
+const s_element = document.getElementById('spotify_element')
+const w_element = document.getElementById('weather_element')
+
+var manual_hide = false
 
 function getDateTime(){
-    fetch('../../../api/display_api.php')
-    .then(response => response.json())
-    .then(data => {
-        hours.innerHTML = data.hours
-        minutes.innerHTML = data.minutes
-        day_name.innerHTML = data.day_name
-        date.innerHTML = data.day + " / " + data.month + " / " + data.year
-    })
+    const d = new Date();
+
+    let day = d.getDate();
+    let month = d.getMonth();
+    let year = d.getFullYear();
+    let hour = d.getHours();
+    let minutess = d.getMinutes();
+
+    if (day < 10) {day = "0" + day}
+    if (month < 10) {month = "0" + month}
+    if (hour < 10) {hour = "0" + hour}
+    if (minutess < 10) {minutess = "0" + minutess}
+    
+    let day2 = d.getDay();
+
+
+    hours.innerHTML = hour
+    minutes.innerHTML = minutess
+    // day_name.innerHTML = data.day_name
+    date.innerHTML = day + " / " + month + " / " + year
 }
 
 function getWeather(){
@@ -38,23 +57,61 @@ function getSpotify(){
     .then(response => response.json())
     .then(data => {
         if(data == null || data.is_playing == false){
-            console.log('nothing is playing')
+            s_element.style.animation = "s_hide 2s ease forwards"
         }else{
             var artistss = ''
+            var songname = ''
             data.item.artists.forEach(function(artist){
                 artistss = artist.name + ', ' + artistss
                 
             })
-            console.log('_ARTIST: ' + artistss.slice(0, -2))
-            console.log('_NAME: ' + data.item.name)
-            console.log('_IMG: ' + data.item.album.images[0].url)
+            if(data.item.name.length > 40){
+                songname = data.item.name.substring(0, 40) + '...'
+            }else{
+                songname = data.item.name
+            }
+            s_image.setAttribute("src", data.item.album.images[0].url)
+            s_artist.innerHTML = artistss.slice(0, -2)
+            s_track.innerHTML = songname
+            if(!manual_hide){
+                s_element.style.animation = "s_show 2s ease forwards"
+            }
+            // console.log('_ARTIST: ' + artistss.slice(0, -2))
+            // console.log('_NAME: ' + data.item.name)
+            // console.log('_IMG: ' + data.item.album.images[0].url)
             // console.log('artist: ' + data.item.artists)
         }
     })
 }
 
+function manualHide(){
+    if(!manual_hide){
+        w_element.style.animation = "w_show 2s ease forwards"
+    }else{
+        w_element.style.animation = "w_hide 2s ease forwards"
+        s_element.style.animation = "s_hide 2s ease forwards"
+    }
+}
+
+document.onkeypress = function (e) {
+    if(e.keyCode == 32){
+        if(manual_hide){
+            manual_hide = false
+            manualHide()
+        }else{
+            manual_hide = true
+            manualHide()
+        }
+    }
+    // use e.keyCode
+};
+
 getSpotify()
+setInterval(getSpotify, 1000)
+
 getWeather()
+setInterval(getWeather, 1000)
+
 getDateTime();
-// setInterval(getData, 30000)
+setInterval(getDateTime, 1000)
 
--- public/theme/home/index.php
+++ public/theme/home/index.php
@@ -4,15 +4,24 @@
         <h2 class="day_name"><span id="day_name"></span></h2>
         <h3 class="date"><span id="date"></span></h3>
     </div>
-    <div class="weather">
+    <div class="weather" id="weather_element">
         <div class="weather_today">
             <div class="w_left">
-                <img src="../../assets/img/weather/clear-day.svg" width="100px" alt="" id="w_img">
+                <img src="../../assets/img/weather/clear-day.svg" alt="" id="w_img">
             </div>
             <div class="w_right">
                 <h1 id="w_temp">24°</h1>
                 <h2 id="w_place">Tvrdošín</h2>
             </div>
+        </div>
+    </div>
+    <div class="spotify" id="spotify_element">
+        <div class="thumbnail">
+            <img src="" id="thumbnail_img" alt="">
+        </div>
+        <div class="s_data">
+            <h3><span id="track_name"></span></h3>
+            <h4><span id="artistname"></span></h4>
         </div>
     </div>
     
